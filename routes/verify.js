e037162bdf1154bf14de6f6caff7b936a1637268
xieke <xiekexk@hfbank.com.cn>
2017-10-09 15:07:56 +0800

    update sth

--- routes/verify.js
+++ routes/verify.js
@@ -6,18 +6,21 @@ var hash = crypto.createHash('sha1');
 
 router.get('/', function (req, res) {
 	//1.将tampstap,nonce,token,echostr按字典序排序
-	var timestamp=req.query.timestamp;
-	var nonce=req.query.nonce;
 	var token='xiaoke';
-	var signature=req.query.signature;
-	var echostr=req.query.echostr;
+	var signature = req.query.signature;
+	var timestamp = req.query.timestamp;
+	var echostr   = req.query.echostr; 
+	var nonce     = req.query.nonce;
 
 	// 2、将三个参数进行字典序排序
 	var arr=[timestamp,nonce,token];
 	arr.sort();
-	
+	console.log(arr)
+	var arrstr =arr.join('');
+	console.log(arrstr)
 	// 3、将三个参数拼接成一个字符串进行sha1加密
-	var sign=hash.update(arr.join('')).digest('hex');
+	var sign = hash.update(arrstr).digest('hex');
+	console.log(sign)
 
 	// 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
 	if (signature===sign) {
