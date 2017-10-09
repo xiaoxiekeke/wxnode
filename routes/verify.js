2b318affe9d0dc214600897ddfcd9b87a337ac8a
xieke <xiekexk@hfbank.com.cn>
2017-10-09 14:44:42 +0800

    .

--- routes/verify.js
+++ routes/verify.js
@@ -15,10 +15,9 @@ router.get('/', function (req, res) {
 	// 2、将三个参数进行字典序排序
 	var arr=[timestamp,nonce,token];
 	arr.sort();
-	console.log(arr)
-	res.send(arr)
+	
 	// 3、将三个参数拼接成一个字符串进行sha1加密
-	var sign=hash.update(arr.join('').digest('hex'));
+	var sign=hash.update(arr.join('')).digest('hex');
 
 	// 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
 	if (signature===sign) {
