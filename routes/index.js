55e14d689894d3e62a0bb186ae41f421e1223087
xieke <xiekexk@hfbank.com.cn>
2017-10-11 15:05:01 +0800

    update sth

--- routes/index.js
+++ routes/index.js
@@ -44,57 +44,57 @@ var funGetContentType = function (filePath) {
 
 module.exports = function (app) {
 	// 对网站首页的访问返回 "Hello World!" 字样
-	// app.get('/', function (req, res) {
-	// 	//获取请求的url 
- //    var reqUrl = req.url;
+	app.get('/', function (req, res) {
+		//获取请求的url 
+    var reqUrl = req.url;
     
- //    //向控制台输出请求的路径 
- //    console.log("1、",reqUrl);
+    //向控制台输出请求的路径 
+    console.log("1、",reqUrl);
     
- //    //使用url解析模块获取url中的路径名 
- //    var pathName = libUrl.parse(reqUrl).pathname;
- //    console.log("2、",pathName);    
- //    // if (libPath.extname(pathName) == "") {
- //    //     //如果路径没有扩展名 
- //    //     pathName += "/"; //指定访问目录 
- //    //     console.log("3、",pathName); 
- //    // }
- //    if (pathName.charAt(pathName.length - 1) == "/") {
- //        //如果访问目录 
- //        pathName += "index.html"; //指定为默认网页
- //        console.log("4、",pathName);  
- //    }
- //    console.log("5、",pathName);  
+    //使用url解析模块获取url中的路径名 
+    var pathName = libUrl.parse(reqUrl).pathname;
+    console.log("2、",pathName);    
+    // if (libPath.extname(pathName) == "") {
+    //     //如果路径没有扩展名 
+    //     pathName += "/"; //指定访问目录 
+    //     console.log("3、",pathName); 
+    // }
+    if (pathName.charAt(pathName.length - 1) == "/") {
+        //如果访问目录 
+        pathName += "index.html"; //指定为默认网页
+        console.log("4、",pathName);  
+    }
+    console.log("5、",pathName);  
     
- //    //使用路径解析模块,组装实际文件路径 
- //    var filePath = libPath.join("/WebRoot", pathName);
- //    console.log("6、",filePath); 
- //    //判断文件是否存在 
- //    libFs.exists(filePath, function (exists) {
- //        //文件存在
- //        if (exists) {
- //            //在响应头中写入内容类型 
- //            res.writeHead(200, { "Content-Type": funGetContentType(filePath) });
+    //使用路径解析模块,组装实际文件路径 
+    var filePath = libPath.join("/WebRoot", pathName);
+    console.log("6、",filePath); 
+    //判断文件是否存在 
+    libFs.exists(filePath, function (exists) {
+        //文件存在
+        if (exists) {
+            //在响应头中写入内容类型 
+            res.writeHead(200, { "Content-Type": funGetContentType(filePath) });
             
- //            //创建只读流用于返回 
- //            var stream = libFs.createReadStream(filePath, { flags: "r", encoding: null });
+            //创建只读流用于返回 
+            var stream = libFs.createReadStream(filePath, { flags: "r", encoding: null });
             
- //            //指定如果流读取错误,返回404错误 
- //            stream.on("error", function () {
- //                res.writeHead(404);
- //                res.end("<h1>404 Read Error</h1>");
- //            });
+            //指定如果流读取错误,返回404错误 
+            stream.on("error", function () {
+                res.writeHead(404);
+                res.end("<h1>404 Read Error</h1>");
+            });
             
- //            //连接文件流和http返回流的管道,用于返回实际Web内容 
- //            stream.pipe(res);
- //        }
- //        else {
- //            //文件不存在，返回404错误 
- //            res.writeHead(404, { "Content-Type": "text/html" });
- //            res.end("<h1>404 Not Found</h1>");
- //        }
- //    });
-	// });
+            //连接文件流和http返回流的管道,用于返回实际Web内容 
+            stream.pipe(res);
+        }
+        else {
+            //文件不存在，返回404错误 
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>404 Not Found</h1>");
+        }
+    });
+	});
 
 	// 网站首页接受 POST 请求
 	app.post('/', function (req, res) {
