b276e625b4359d13e76b21abd4ba64293c8ae4a2
xieke <xiekexk@hfbank.com.cn>
2017-10-13 16:34:47 +0800

    update sth

--- routes/verify.js
+++ routes/verify.js
@@ -323,7 +323,7 @@ router.post('/setWxMenu', function (req, res) {
 				url: proxy_url,
 		    method: req.method.toUpperCase(),
 		    json: true,
-		    body: req.query
+		    form: req.query
 			},function(error, response, data){
 				if (!error && response.statusCode == 200) {
 		      console.log('------接口数据------',data);
