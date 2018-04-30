9152983c0fbeaee286be96d28d61c13a0ed5074e
xieke <xiekexk@hfbank.com.cn>
2018-04-30 15:47:32 +0800

    update sth

--- routes/index.js
+++ routes/index.js
@@ -54,8 +54,8 @@ module.exports = function (app) {
 	});
 
   app.use('/verify', require('./verify'));
-  // app.use('/be', require('./be'));
-  app.use('/api', require('./fe'));
+  app.use('/be', require('./be'));
+  // app.use('/api', require('./fe'));
   // app.use('/signup', require('./signup'));
   // app.use('/signin', require('./signin'));
   // app.use('/signout', require('./signout'));
