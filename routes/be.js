0d0929e4c1886707738aa91748127c221d2a8ab1
xieke <xiekexk@hfbank.com.cn>
2018-05-27 01:49:30 +0800

    update sth

--- routes/be.js
+++ routes/be.js
@@ -857,9 +857,9 @@ router.post('/chapter/list', middleware.hasToken,function (req, res) {
 		Chapter.find().sort({'meta.createAt':-1}).exec().then(function(result){
 			res.status(200).send({
 				result:errConfig.success,
-				// data:{
-				// 	list:result
-				// }
+				data:{
+					list:result
+				}
 			});
 		},function(err){
 	  	res.status(200).send({
